f389dbf10b152c4cd7930f201c1583aac0c36c23
smitikshah <smitikshah@gmail.com>
2018-03-16 21:38:46 -0400

    Refrence

---
+++ Modding My mods/src/main/java/me/Yankeefan002/First/FirstMod.java
@@ -0,0 +1,26 @@
+package me.Yankeefan002.First;
+
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = "")
+public class FirstMod {
+	
+	@EventHandler
+	public void preInit(FMLPreInitializationEvent event) 
+	{
+		
+	}
+	@EventHandler
+	public void Init(FMLInitializationEvent event) 
+	{
+		
+	}
+	@EventHandler
+	public void PostInit(FMLPostInitializationEvent event) 
+	{
+		
+	}
+}
---
+++ Modding My mods/src/main/java/me/Yankeefan002/First/Refrence.java
@@ -0,0 +1,10 @@
+package me.Yankeefan002.First;
+
+public class Refrence {
+
+	public static final String MOD_ID = "FM";
+	public static final String MOD_NAME = "FirstMod";
+	public static final String VERSION = "1.0";
+
+
+}
