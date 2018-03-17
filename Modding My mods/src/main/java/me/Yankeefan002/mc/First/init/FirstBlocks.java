17be98ec64536459b2d6c3b4866555908b8778c9
smitikshah <smitikshah@gmail.com>
2018-03-17 10:11:40 -0400

    Item item phase

--- Modding My mods/src/main/java/me/Yankeefan002/mc/First/init/FirstBlocks.java
+++ Modding My mods/src/main/java/me/Yankeefan002/mc/First/init/FirstBlocks.java
@@ -1,7 +1,13 @@
 package me.Yankeefan002.mc.First.init;
 
+import me.Yankeefan002.First.Refrence;
+import me.Yankeefan002.First.blocks.BlockTest;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.resources.model.ModelResourceLocation;
+import net.minecraft.item.Item;
+import net.minecraftforge.fml.common.registry.GameRegistry;
 
 public class FirstBlocks {
 	
@@ -9,12 +15,22 @@ public class FirstBlocks {
 	
 	public static void init() 
 	{
-		Monkey_Block = new Block(Material.cloth).setUnlocalizedName("Monkey_Block");
+		Monkey_Block = new BlockTest(Material.cloth).setUnlocalizedName("Monkey_Block");
 	}
 
+	
 	public static void register() 
+	{
+		GameRegistry.registerBlock(Monkey_Block, Monkey_Block.getUnlocalizedName().substring(5));
+	}
+
+	public static void registerRenders() 
 	{
 		
 	}
-
+	public static void registerRender(Block block) 
+	{
+		Item item = Item.getItemFromBlock(block);
+		Minecraft.getMinecraft().getRenderItem().getItemModelMesher().register(item, 0, new ModelResourceLocation(Refrence.MOD_ID+ ";" + item.getUnlocalizedName().substring(5), "inventory"));
+	}
 }
